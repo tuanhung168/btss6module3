9858498286c97022f4a6b02fd178866013d24661
tuanhung168 <96073469+tuanhung168@users.noreply.github.com>
2023-06-12 00:51:56 +0700

    Add files via upload

---
+++ btss6/src/Bt1/src/Main.java
@@ -0,0 +1,20 @@
+import javafx.scene.shape.Circle;
+
+public class Main {
+    public static void main(String[] args) {
+        Circle circle = new Circle(5.0, "red");
+
+        System.out.println("Bán kính: " + circle.getRadius());
+        System.out.println("Màu sắc: " + circle.getColor());
+
+        circle.setRadius(3.0);
+        circle.setColor("blue");
+
+        System.out.println("Chu vi: " + circle.chuVi());
+        System.out.println("Diện tích: " + circle.dienTich());
+
+        circle.inputData();
+
+        circle.displayData();
+    }
+}
